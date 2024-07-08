4881e6a5ba8b23ab2caee1d68dd55b46c324fae7
Octavio Paz <octavioemanuelpaz@gmail.com>
2024-07-08 14:10:11 -0300

    Update api.js
    lista para deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
- //baseURL: 'https://fake-api-seven-nu.vercel.app' //descomentar  para usar base de datos fake de vercel
- baseURL: 'http://localhost:3000' // descomentar para usar base de datos local
+ baseURL: 'https://fake-api-seven-nu.vercel.app' //descomentar  para usar base de datos fake de vercel
+ //baseURL: 'http://localhost:3000' // descomentar para usar base de datos local
 });
 
 export default api;
